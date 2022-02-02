c4927ac7eb86d73d166f96d39856c71d74fdadb8
Stephen Harrold <steharrold@gmail.com>
2022-02-02 21:30:48 +0000

    setup s3 bucket for static and media files

--- shoop/settings.py
+++ shoop/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
     # installed apps
     'crispy_forms',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -173,6 +174,24 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 # Media files (CSS, JavaScript, Images)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+if 'USE_AWS' in os.environ:
+    # Bucket Config
+    AWS_STORAGE_BUCKET_NAME = 'ms4-shoop'
+    AWS_S3_REGION_NAME = 'eu-west-1'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_SECRET_ACCESS_KEY_ID')
+    AWS_S3_CUSTOM_DOMAIN = f'.s3.amazonaws.com'
+
+    # Static and media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # Override static and media URLSs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
