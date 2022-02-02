a258bda2529f5fbe0616bbfff36553629e3cddca
Stephen Harrold <steharrold@gmail.com>
2022-02-02 20:35:17 +0000

    hide secret key

--- shoop/settings.py
+++ shoop/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hz@(i#+u+cl*h9%!rv=6aegthezb+@kunbx(54dk92us2p$b+u'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
