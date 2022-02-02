626292f567969b551db9f084d20ec5781e413b53
Stephen Harrold <steharrold@gmail.com>
2022-02-02 18:39:12 +0000

    add refine search for filters and sort by price

--- products/views.py
+++ products/views.py
@@ -1,18 +1,21 @@
+from unicodedata import category
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
-from .models import Shoe
+from .models import Shoe, Brand, Category
 from reviews.models import Review
 from django.db.models import Q, Avg
-from django.db.models.functions import Round
 
 
 def all_shoes(request):
     """ A view to allow user to browse, sort and search shoes """
     shoes = Shoe.objects.all()
     header = "Browse - All"
+    categories = Category.objects.all(),
+    brands = Brand.objects.all(),
 
     # get handler
     if request.GET:
+
         # search box queries
         if 'q' in request.GET:
             query = request.GET['q']
@@ -25,6 +28,7 @@ def all_shoes(request):
                     category__name__icontains=query)
             shoes = shoes.filter(queries)
             header = f'Search - "{query}"'
+
         # gender queries
         if 'gender' in request.GET:
             gender = request.GET['gender']
@@ -43,9 +47,31 @@ def all_shoes(request):
         else:
             extracted_shoe.avg_rating = round(avg_rating['rating__avg'])
 
+    # handle refined searches
+    if 'filter_category' in request.GET:
+        query = request.GET['filter_category']
+        if query != 'all':
+            category = Category.objects.get(name=query)
+            shoes = shoes.filter(category=category)
+
+    if 'filter_brand' in request.GET:
+        query = request.GET['filter_brand']
+        if query != 'all':
+            brand = Brand.objects.get(name=query)
+            shoes = shoes.filter(brand=brand)
+
+    if 'sort_price' in request.GET:
+        query = request.GET['sort_price']
+        if query == 'desc':
+            shoes = shoes.order_by('-price')
+        else:
+            shoes = shoes.order_by('price')
+
     context = {
         'shoes': shoes,
         'header': header,
+        'categories': categories,
+        'brands': brands,
     }
 
     template = 'products/browse.html'
