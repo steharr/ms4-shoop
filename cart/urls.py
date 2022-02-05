f4ec224a04bee1587a881d0fff4cdc494c623d75
Stephen Harrold <steharrold@gmail.com>
2022-02-05 16:07:56 +0000

    add update and delete from cart functionality

--- cart/urls.py
+++ cart/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.view_cart, name='cart'),
     path('add/<shoe_id>/', views.add_to_cart, name="add_to_cart"),
+    path('remove/<shoe_id>/', views.remove_from_cart, name="remove_from_cart"),
+    path('update/<shoe_id>/', views.update_cart, name="update_cart"),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,4 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from products.models import Shoe
 
@@ -9,9 +10,12 @@ def view_cart(request):
 
 def add_to_cart(request, shoe_id):
     """ Add a specified shoe to the cart """
+
+    # extract the cart object
+    cart = request.session.get('cart', {})
+
     shoe = get_object_or_404(Shoe, pk=shoe_id)
     size = request.POST.get('shoe_size')
-    cart = request.session.get('cart', {})
     redirect_url = request.POST.get('redirect_url')
 
     # check if shoe is already in the cart
@@ -32,3 +36,46 @@ def add_to_cart(request, shoe_id):
 
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+
+def remove_from_cart(request, shoe_id):
+    """ Remove a specified shoe in the cart """
+    try:
+        # extract the cart object
+        cart = request.session.get('cart', {})
+
+        # find the specified size  from the request
+        size = request.POST['size']
+
+        # remove the specified size in the server cart variable
+        del cart[shoe_id][size]
+
+        # update the client session cart
+        request.session['cart'] = cart
+
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
+
+
+def update_cart(request, shoe_id):
+    """ update the qty of a specified shoe in the cart """
+    try:
+        # extract the cart object
+        cart = request.session.get('cart', {})
+
+        # find the specified size and new qty from the request
+        size = request.POST['size']
+        new_qty = request.POST['qty']
+
+        # update the server cart variable with new qty
+        cart[shoe_id][size] = {'qty': int(new_qty)}
+
+        # update the client session cart
+        request.session['cart'] = cart
+
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
