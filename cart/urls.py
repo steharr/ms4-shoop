0fa5f40d218c327a32dd0a6c296907f16040eaee
Stephen Harrold <steharrold@gmail.com>
2022-02-05 10:41:24 +0000

    add views for view and add to shopping cart

--- cart/urls.py
+++ cart/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.view_cart, name='cart'),
+    path('<int:shoe_id>', views.add_to_cart, name="add_to_cart"),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from products.models import Shoe
 
-# Create your views here.
+
+def view_cart(request):
+    """ View for user to see whats in their shopping cart """
+    template = ' cart/cart.html'
+    return render(request, template)
+
+
+def add_to_cart(request, shoe_id):
+    """ Add a specified shoe to the cart """
+    shoe = get_object_or_404(Shoe, pk=shoe_id)
+    size = request.POST.get('shoe_size')
+    cart = request.session.get('cart', {})
+    redirect_url = request.POST.get('redirect_url')
+
+    if shoe_id in list(cart.keys()):  # check if shoe is already in the cart
+        cart[shoe_id]['qty'] += 1
+        cart[shoe_id]['size'] = size
+    else:
+        cart[shoe_id]['qty'] = 1
+        cart[shoe_id]['size'] = size
+
+    request.session['cart'] = cart
+    return redirect(redirect_url)
