e7324627aa3347ad3a3d5d75924016cbdb5af8be
Stephen Harrold <steharrold@gmail.com>
2022-02-06 15:26:54 +0000

    add link to checkout app

--- cart/urls.py
+++ cart/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from checkout.views import checkout_preview
 
 urlpatterns = [
     path('', views.view_cart, name='cart'),
     path('add/<shoe_id>/', views.add_to_cart, name="add_to_cart"),
     path('remove/<shoe_id>/', views.remove_from_cart, name="remove_from_cart"),
     path('update/<shoe_id>/', views.update_cart, name="update_cart"),
+    path('checkout/preview/', checkout_preview, name='checkout_preview'),
 ]
