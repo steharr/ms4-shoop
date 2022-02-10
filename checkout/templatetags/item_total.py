47a6efd7a8cac9f25580e4916c201866ee32848e
Stephen Harrold <steharrold@gmail.com>
2022-02-10 19:33:08 +0000

    add template tag for line item total calc

---
+++ checkout/templatetags/item_total.py
@@ -0,0 +1,11 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter(name="item_total")
+def item_total(price, qty):
+    """ 
+    Returns the item cost
+    """
+    return price * qty
