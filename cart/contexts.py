19d8f33d51cb906fd887c5f0760a8046712bc681
Stephen Harrold <steharrold@gmail.com>
2022-02-12 17:42:04 +0000

    add order total, delivery cost to context processor and templates

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,18 +1,20 @@
 from products.models import Shoe
 from django.shortcuts import get_object_or_404
+from decimal import Decimal
 
 
 def cart_contents(request):
     cart = request.session.get('cart', {})
     cart_items = []
-    grand_total = 0
+    order_total = 0
+    delivery_cost = Decimal(3.00)
 
     for shoe_id, shoe_data in cart.items():
         shoe = get_object_or_404(Shoe, pk=shoe_id)
         for chosen_size in shoe_data:
             qty = shoe_data[chosen_size]['qty']
             item_price = shoe.price * qty
-            grand_total += item_price
+            order_total += item_price
             cart_items.append({
                 'shoe_id': shoe_id,
                 'shoe': shoe,
@@ -20,6 +22,12 @@ def cart_contents(request):
                 'qty': qty,
                 'item_price': item_price
             })
-    context = {'cart_items': cart_items, 'grand_total': grand_total}
+
+    context = {
+        'cart_items': cart_items,
+        'order_total': order_total,
+        'delivery_cost': delivery_cost,
+        'grand_total': delivery_cost + order_total
+    }
 
     return context
