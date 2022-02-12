2423a38bbafb3a9f3087de2597830bb590bf1f4e
Stephen Harrold <steharrold@gmail.com>
2022-02-12 15:44:33 +0000

    fix escape character so euro symbol is rendered in template

--- banners/contexts.py
+++ banners/contexts.py
@@ -6,7 +6,7 @@ def sales_banners(request):
     banner = Banner.objects.get(
         pk=3)  # there should only ever be one banner pk=1
     if banner:
-        full_banner_message = f"{banner.message}: {str(banner.discount)}% off all orders over Euro {str(banner.price_threshold)}"
+        full_banner_message = f"{banner.message}: {str(banner.discount)}% off all orders over &euro; {str(banner.price_threshold)}"
         banner_context = {
             'message': banner.message,
             'theme': banner.theme,
